186706a0784f377cf7cc66e8b9118a65b57a4093
Lucas Batista <contact@lucasbatista.me>
2024-09-22 11:55:47 -0300

    Play online

--- lib/views/components/main_menu_view/game_options/game_mode_picker.dart
+++ lib/views/components/main_menu_view/game_options/game_mode_picker.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/cupertino.dart';
-
 import 'package:en_passant/views/components/main_menu_view/game_options/picker.dart';
+import 'package:flutter/cupertino.dart';
 
 class GameModePicker extends StatelessWidget {
   final Map<int, Text> playerCountOptions = const <int, Text>{
     1: Text('One Player'),
     2: Text('Two Player'),
+    3: Text('Online'),
   };
 
   final int playerCount;
